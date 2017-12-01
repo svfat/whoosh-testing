fd94275ceb25484a3af1b7f20a0844c12ec9b5ea
Stan Fateev <stanfateev@gmail.com>
2017-12-01 20:30:36 +0700

    some comments

--- main.py
+++ main.py
@@ -44,14 +44,21 @@ def get_index(directory):
     return ix
 
 def main():
-    ix = get_index(config.INDEXDIR_PATH)
+    ix = get_index(config.INDEXDIR_PATH) # get document index
+
+    # creating QueryParser
     qp = QueryParser("body", schema=ix.schema, group=OrGroup.factory(0.9))
+    # we will use FuzzyTermPlugin if nothing was found with exact search
     qp.add_plugin(FuzzyTermPlugin())
+
     for sentence in SENTENCES:
         q = qp.parse(sentence)
         results = None
         with ix.searcher() as s:
             if config.CORRECTOR:
+                # experiment with internal whoosh corrector
+                # set CORRECTOR=True in config.py to use
+                # I've got weird results, though
                 corrected = s.correct_query(q, sentence)
                 if corrected.query != q:
                     print("Did you mean:", corrected.string)
@@ -66,12 +73,13 @@ def main():
             # very ugly solution, maybe we can use Whoosh stemming abilities
             # http://whoosh.readthedocs.io/en/latest/stemming.html
             chunks = sentence.split()
-            sentence = '~ '.join(chunks)+'~'
+            sentence = '~ '.join(chunks)+'~' # add ~ to each word
             q = qp.parse(sentence)
             with ix.searcher() as s:
                 query = q
                 info = 'FUZZY ' + sentence
                 results = [r['body'] for r in s.search(query, terms=True)]
+                
         print('BM25F scoring:', info, '->', results)
             #print('terms matched:', [r.matched_terms() for r in results])
     #    with ix.searcher(weighting=scoring.TF_IDF()) as s:
