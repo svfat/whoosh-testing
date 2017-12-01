ce958d4e4b981e3d30ba6d636e7a769ad9039cb4
Stan Fateev <stanfateev@gmail.com>
2017-12-01 19:47:30 +0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # whoosh-testing
 Testing Whoosh framework
+
+> pip install -R requirements.txt
+
+> python main.py
---
+++ main.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+import os
+
+import whoosh.index as index
+from whoosh.qparser import QueryParser, OrGroup, FuzzyTermPlugin
+from whoosh.fields import *
+from whoosh import scoring
+
+INDEXDIR_PATH = "indexdir"
+CORRECTOR = False
+SENTENCES = ["how are yoou",
+             "chateu lator meru",
+             "chateau lator",
+             "chateau latour",
+             "chateau latour merus",
+             "blak opul"]
+
+
+def create_dir(directory):
+    """
+    Create directory if not exists
+    """
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+def create_index(directory):
+    """
+    Generate Whoosh index from text file
+    """
+    schema = Schema(body=TEXT(stored=True))
+    create_dir(directory)
+    ix = index.create_in(directory, schema)
+    writer = ix.writer()
+    with open('magia_dictionary_en.txt', 'r') as f:
+        for line in f:
+            writer.add_document(body=line.strip())
+    writer.commit()
+    return ix
+
+def get_index(directory):
+    """
+    Return Whoosh index
+    """
+    try:
+        ix = index.open_dir(directory)
+    except:
+        ix = create_index(directory)
+        ix = index.open_dir(directory)
+    return ix
+
+def main():
+    ix = get_index(INDEXDIR_PATH)
+    qp = QueryParser("body", schema=ix.schema, group=OrGroup.factory(0.9))
+    qp.add_plugin(FuzzyTermPlugin())
+    for sentence in SENTENCES:
+        q = qp.parse(sentence)
+        with ix.searcher() as s:
+            if CORRECTOR:
+                corrected = s.correct_query(q, sentence)
+                if corrected.query != q:
+                    print("Did you mean:", corrected.string)
+                query = corrected.query
+                q_str = corrected.string
+            else:
+                query = q
+                q_str = sentence
+            results = s.search(query, terms=True)
+            print('BM25F scoring:', q_str, '->', [r['body'] for r in results])
+            #print('terms matched:', [r.matched_terms() for r in results])
+    #    with ix.searcher(weighting=scoring.TF_IDF()) as s:
+    #        corrected = s.correct_query(q, sentence)
+    #        if corrected.query != q:
+    #            print("Did you mean:", corrected.string)
+    #        results = s.search(q, terms=True)
+    #        print('TD-IDF scoring:', sentence, '->', [r['body'] for r in results])
+            #print('terms matched:', [r.matched_terms() for r in results])
+
+        print('--------------')
+if __name__ == "__main__":
+    main()
