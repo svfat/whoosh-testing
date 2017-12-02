a910f4610e00ef4193c7337c6ae293c831219a6e
Stan Fateev <stanfateev@gmail.com>
2017-12-02 13:43:11 +0700

    test data from csv

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 INDEXDIR_PATH = "indexdir"
+DICTIONARY_FILENAME = "magia_dictionary_en.txt"
 CORRECTOR = False
+TEST_DATA_CSV="test_data.csv"
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
+import csv
 
 import whoosh.index as index
 from whoosh.qparser import QueryParser, OrGroup, FuzzyTermPlugin
@@ -10,13 +11,13 @@ import config
 from data import SENTENCES
 
 
-
 def create_dir(directory):
     """
     Create directory if not exists
     """
     if not os.path.exists(directory):
         os.makedirs(directory)
+
 
 def create_index(directory):
     """
@@ -32,6 +33,7 @@ def create_index(directory):
     writer.commit()
     return ix
 
+
 def get_index(directory):
     """
     Return Whoosh index
@@ -43,15 +45,28 @@ def get_index(directory):
         ix = index.open_dir(directory)
     return ix
 
+
+def get_test_data(csv_file):
+    result = []
+    with open(csv_file, 'r') as f:
+        rdr = csv.DictReader(f)
+        for row in rdr:
+            result.append((row['Sentence'], row['Expected Slot']))
+    return result
+
+
 def main():
-    ix = get_index(config.INDEXDIR_PATH) # get document index
+    ix = get_index(config.INDEXDIR_PATH)  # get document index
 
     # creating QueryParser
     qp = QueryParser("body", schema=ix.schema, group=OrGroup.factory(0.9))
     # we will use FuzzyTermPlugin if nothing was found with exact search
     qp.add_plugin(FuzzyTermPlugin())
 
-    for sentence in SENTENCES:
+    # test_data = SENTENCES
+    test_data = get_test_data(config.TEST_DATA_CSV)
+
+    for sentence, expected in test_data:
         q = qp.parse(sentence)
         results = None
         with ix.searcher() as s:
@@ -73,23 +88,25 @@ def main():
             # very ugly solution, maybe we can use Whoosh stemming abilities
             # http://whoosh.readthedocs.io/en/latest/stemming.html
             chunks = sentence.split()
-            sentence = '~ '.join(chunks)+'~' # add ~ to each word
+            sentence = '~ '.join(chunks) + '~'  # add ~ to each word
             q = qp.parse(sentence)
             with ix.searcher() as s:
                 query = q
                 info = 'FUZZY ' + sentence
                 results = [r['body'] for r in s.search(query, terms=True)]
-                
+
         print('BM25F scoring:', info, '->', results)
-            #print('terms matched:', [r.matched_terms() for r in results])
-    #    with ix.searcher(weighting=scoring.TF_IDF()) as s:
-    #        corrected = s.correct_query(q, sentence)
-    #        if corrected.query != q:
-    #            print("Did you mean:", corrected.string)
-    #        results = s.search(q, terms=True)
-    #        print('TD-IDF scoring:', sentence, '->', [r['body'] for r in results])
-            #print('terms matched:', [r.matched_terms() for r in results])
+        # print('terms matched:', [r.matched_terms() for r in results])
+        #    with ix.searcher(weighting=scoring.TF_IDF()) as s:
+        #        corrected = s.correct_query(q, sentence)
+        #        if corrected.query != q:
+        #            print("Did you mean:", corrected.string)
+        #        results = s.search(q, terms=True)
+        #        print('TD-IDF scoring:', sentence, '->', [r['body'] for r in results])
+        # print('terms matched:', [r.matched_terms() for r in results])
 
         print('--------------')
+
+
 if __name__ == "__main__":
     main()
